2db996a03026386fa69a50444cb4673c6a36526d
NicoBarragan <nbarragan01@gmail.com>
2022-04-06 04:10:14 -0300

    feature:(contracts/LiquidityPool.sol): added number of blocks providing liquidity reward when distributing the governance token

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -15,34 +15,37 @@ contract LiquidityPool {
     LpToken public lpToken;
     GovernanceToken public govToken;
     uint public tokenAmount;
-
-    // mapping (address => uint) public poolTokenProvided;
-    // mapping (address => uint) public lpTokenProvided;
+    uint public govAmount;
+    uint public constant GOV_PER_BLOCK = 1;
 
     constructor(address _poolToken, address _lpToken, address _govToken) public {
         poolToken = PoolToken(_poolToken);
         lpToken = LpToken(_lpToken);
         govToken = GovernanceToken(_govToken);
-
     }
+
+    struct TxProvided {
+        uint amount;
+        uint blockNumber;
+    }
+    mapping (address => TxProvided) public blockTokenProvided;
 
     function depositPool(uint _amount) external {
         require(_amount > 0, "Amount must be greater than 0");
         poolToken.transferFrom(msg.sender, address(this), _amount);
-        // poolTokenProvided[msg.sender] += _amount;
+        blockTokenProvided[msg.sender] = TxProvided(_amount, block.number);
         lpToken.mint(msg.sender, _amount);
     }
 
-
-    function withdrawPool(uint _lpAmount) external {
-        require(_lpAmount > 0, "Amount must be greater than 0");
-        uint amountToReturn = _lpAmount / lpToken.getDividend();
-        lpToken.transferFrom(msg.sender, address(this), _lpAmount);
-        // poolTokenProvided[msg.sender] -= tokenAmount;
+    function withdrawPool() external {
+        require(blockTokenProvided[msg.sender].amount > 0, "No tokens provided");
+        uint amountToReturn = blockTokenProvided[msg.sender].amount / lpToken.getDividend();
+        uint govAmount = blockTokenProvided[msg.sender].amount * ((block.number - blockTokenProvided[msg.sender].blockNumber) * GOV_PER_BLOCK);
+        
+        lpToken.transferFrom(msg.sender, address(this), blockTokenProvided[msg.sender].amount);
         
         poolToken.transfer(msg.sender, amountToReturn);
-        govToken.mint(msg.sender, _lpAmount);
+        govToken.mint(msg.sender, govAmount);
     }
-
 
 }
